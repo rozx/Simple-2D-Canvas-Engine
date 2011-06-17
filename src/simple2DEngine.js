bc65185e1b4ce0facbab0ab42983428a6fc838c0
rozx <lisida900710@gmail.com>
2011-06-17 12:30:34 +0800

    version 0.02

--- src/simple2DEngine.js
+++ src/simple2DEngine.js
@@ -1,5 +1,10 @@
+			var IMAGE = 0 , TEXT = 1;
+			
 			function Director(){
-				this.MaxFPS = 60;
+				
+
+				
+				this.MaxFPS = 120;
 				this.ShowFPS = true;
 				this.Scene = new Scene();
 				this.Canvas = undefined;
@@ -9,8 +14,8 @@
 				
 				this.initTimer = function (DirectorElementName){
 				
-					setInterval(DirectorElementName + ".CountFPS()", 100);
-					setInterval(DirectorElementName + ".Draw()", parseInt(100 / this.MaxFPS));
+					setInterval(DirectorElementName + ".CountFPS()", 1000);
+					setInterval(DirectorElementName + ".Draw()", parseInt(1000 / this.MaxFPS));
 					
 				}
 				
@@ -18,6 +23,9 @@
 				this.setCanvas = function(ElementCanvas){
 					this.Canvas = ElementCanvas;
 					this.Context = ElementCanvas.getContext("2d");
+					
+					this.Scene.Width = this.Canvas.width;
+					this.Scene.Height = this.Canvas.height;
 					
 					console.log("Canvas Setting Done!");
 					console.log(this.Context);
@@ -31,8 +39,16 @@
 					this.FramesDone++;
 					
 					if(this.Context){
-
+						
+						//Sprites Speed++
 					
+						for(var i=0;i<this.Scene.Sprites.length;i++){
+							if(this.Scene.Sprites[i]){
+								this.Scene.Sprites[i].x+= this.Scene.Sprites[i].SpeedX / this.MaxFPS;
+								this.Scene.Sprites[i].y+= this.Scene.Sprites[i].SpeedY / this.MaxFPS;
+							}
+						}
+						
 					
 						this.Context.fillStyle = "black";  
 						
@@ -48,17 +64,34 @@
 						
 						for(var i=0;i<this.Scene.Sprites.length;i++){
 							if(this.Scene.Sprites[i]){
-								this.Context.drawImage(this.Scene.Sprites[i].img,this.Scene.Sprites[i].x,this.Scene.Sprites[i].y);
-								//alert(this.Scene.Sprites[i].img);
+								
+								switch(this.Scene.Sprites[i].SpriteType){
+									case IMAGE:
+									this.Context.drawImage(this.Scene.Sprites[i].img,this.Scene.Sprites[i].x,this.Scene.Sprites[i].y);
+									break;
+									
+									case TEXT:
+									this.Context.fillStyle = this.Scene.Sprites[i].Color;
+									this.Context.font = this.Scene.Sprites[i].Style + " " + this.Scene.Sprites[i].Size + "px " + this.Scene.Sprites[i].Font;
+									this.Context.fillText(this.Scene.Sprites[i].Text,this.Scene.Sprites[i].x,this.Scene.Sprites[i].y);
+									
+									break;
+									//alert(this.Scene.Sprites[i].img);
+								}
 							}
 						}
-						
+					// Call Collition detection method
+						this.Scene.CollideDetect();
+							
+					
+					
 					}  
 				};
 				
 				this.CountFPS = function(){
 					this.FPS = this.FramesDone;
 					this.FramesDone = 0;
+					
 					
 					//console.log("FPS:" + this.FPS);
 				};
@@ -67,11 +100,41 @@
 			}
 			
 			function Scene(){
+				
+				this.Width = undefined;
+				this.Height = undefined;
+				
 				this.Sprites = [];
+				
+				
+				this.CollideDetect = function(){
+					
+						for(var i=0;i<this.Sprites.length;i++){
+							if(this.Sprites[i]){
+								
+								
+								if(this.Sprites[i].x < 0 || (this.Sprites[i].x + this.Sprites[i].Width) > this.Width ){
+									
+									
+									if(this.Sprites[i].OnCollideXEdge){
+										this.Sprites[i].OnCollideXEdge();
+									}
+								}
+								
+								if(this.Sprites[i].y < 0 || (this.Sprites[i].y + this.Sprites[i].Height) > this.Height ){
+									
+									
+									if(this.Sprites[i].OnCollideYEdge){
+										this.Sprites[i].OnCollideYEdge();
+									}
+								}
+						}
+					};
+				}
 				
 				this.appendSprite = function(Sprite,Zid){
 					if(this.Sprites[Zid]){
-						this.appednSprite(Sprite,Zid + 1);
+						this.appendSprite(Sprite,Zid + 1);
 						
 					} else {
 						this.Sprites[Zid] = Sprite; 
@@ -99,12 +162,34 @@
 			}
 			
 			function Sprite(){
+				
 				this.id = undefined;
-				this.img = undefined;
+				this.SpriteType = undefined;
+				
+				
+				this.Width = undefined;
+				this.Height = undefined;
 				this.x = 0;
 				this.y = 0;
 				this.SpeedX = 0;
 				this.SpeedY = 0;
+				
+				// For Text
+				this.Text = undefined;
+				this.Color = undefined;
+				this.Font = undefined;
+				this.Size = undefined;
+				this.Style = undefined;
+				
+				// For Image
+				
+				this.img = undefined;
+				
+				//Collision detection
+				
+				this.OnCollideXEdge = undefined;
+				this.OnCollideYEdge = undefined;
+				
 				
 				this.Create = function(id) {
 					this.id = id;
@@ -115,6 +200,20 @@
 					this.y = y;
 				};
 				
+				this.CreateNewText= function(text, font, color, size , style) {
+					this.Color = color;
+					this.Font = font;   
+					//this.Context.textBaseline = "top";   
+					this.Text = text;   
+					this.SpriteType = TEXT;
+					this.Size = size;
+					this.Style = style;
+					
+					
+					this.Width = parseInt((text.length * this.Size) / 2);
+					this.Height = this.Size;
+				}
+				
 				
 				this.CreateNewImg= function(src) {
 				
@@ -122,16 +221,35 @@
 				
 						var MyBody = document.getElementsByTagName("body")[0];
 						var NewImg = document.createElement("img");
-						NewImg.src=src;
+						
+						NewImg.id = this.id;
+						NewImg.src=src;		
+						
 						NewImg.width = 0;
 						NewImg.height = 0;
-						NewImg.id = this.id;
-					
 					
 						MyBody.appendChild(NewImg);
 					
 						this.img = new Image();
 						this.img.src = src;
+						
+						
+						this.Width = this.img.width;
+						this.Height = this.img.height;
+						
+						console.log(this.img.height);
+						
+
+						
+						this.SpriteType = IMAGE;
 					}
 				};
+				
+				
+				this.SetSpeed = function(SpeedX,SpeedY) {
+					this.SpeedX = SpeedX;
+					this.SpeedY = SpeedY;
+				}
+				
+				
 			}
